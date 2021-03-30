73c0d6c0097bd48cc4b33abd193ef20762837b05
qizheng-jin <386917194@qq.com>
2021-03-30 17:46:39 +0800

    day2_finish

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,5 +1,6 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+public class Test implements Runnable{
+    @Override
+    public void run() {
+
     }
 }
